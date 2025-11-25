af90187216050dfa90fbbb53795be3ef072f2171
DiegoLanda26950 <a26950@svalero.com>
2025-11-25 19:13:10 +0100

    Add files via upload

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -2,35 +2,35 @@ using System.Collections;
 
 namespace Models;
 
-public class Pokemon {
-
-    //Datos
-    public int Pokedex_Id { get; set; }
-    public int Generacion { get; set; }
+public class Pokemon
+{
+    // Datos
+    public int ID { get; set; }
+    public string Region { get; set; }
     public string Nombre { get; set; }
-    public int Peso { get; set; }
+    public double Peso { get; set; }
     public bool Shiny { get; set; }
-    public List<Tipo?> Tipos { get; set; } = new();
-    public List<Movimiento?> Movimientos { get; set; } = new();
-    public List<Habilidad?> Habilidades { get; set; } = new();
-    public Estadistica? Estadisticas { get; set; }
+    public string Tipos { get; set; }
+    public Habilidad Habilidad { get; set; } = new();
+    public Pokeball Pokeball { get; set; }
+    public Habitat Habitat { get; set; }
 
-
-
-    //Constructor
-    public Pokemon(int pokedex_id, int generacion, string nombre, int peso, bool shiny, List<Tipo?> tipos, List<Movimiento?> movimientos, List<Habilidad?> habilidades, Estadistica estadisticas){
-        Pokedex_Id = pokedex_id;
-        Generacion = generacion;
-        Nombre = nombre;
-        Peso = peso;
-        Shiny = shiny;
-        Tipos = tipos;
-        Movimientos = movimientos;
-        Habilidades = habilidades;
-        Estadisticas = estadisticas;
+    // Constructor completo
+    public Pokemon(int id, string region, string nombre, double peso, bool shiny, string tipos, Habilidad habilidad, Pokeball pokeball, Habitat habitat)
+    {
+        this.ID = id;
+        this.Region = region;
+        this.Nombre = nombre;
+        this.Peso = peso;
+        this.Shiny = shiny;
+        this.Tipos = tipos;
+        this.Habilidad = habilidad;
+        this.Pokeball = pokeball;
+        this.Habitat = habitat;
     }
 
-    public Pokemon(){}
-
-
+    // Constructor vacío
+    public Pokemon()
+    {
+    }
 }
