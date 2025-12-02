ac6b04a3cba9585c62fcb05343ae7bfd5d94086c
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-12-02 19:33:31 +0100

    Cambio en el Program y Modificacion de Pokemon_API y settings para MysqlConnection y Linkear nuestra DB

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
+using Pokemon_API.Controllers;
+using Pokemon_API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PokemonDB");
+
+builder.Services.AddScoped<IHabilidadRepository, HabilidadRepository>(provider =>
+new HabilidadRepository(connectionString));
+
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>(provider =>
+new PokemonRepository(connectionString));
+
+builder.Services.AddScoped<IHabitatRepository, HabitatRepository>(provider =>
+new HabitatRepository(connectionString));
+
+builder.Services.AddScoped<IPokeballRepository, PokeballRepository>(provider =>
+new PokeballRepository(connectionString));
+
+builder.Services.AddScoped<IObjetoRepository, ObjetoRepository>(provider =>
+new ObjetoRepository(connectionString));
+
 
 // Add services to the container.
 
@@ -22,4 +42,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+
+//HabilidadController.InicializarDatos();
 app.Run();
